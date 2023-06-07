21f82a8510e92f2025cfc8a1ab10605b63e25d98
josephtule <letujoseph@gmail.com>
2023-06-06 18:52:01 -0600

    fixed some output formatting

--- aoc2022/day1/day1.cpp
+++ aoc2022/day1/day1.cpp
@@ -1,40 +1,44 @@
+#include <algorithm>
+#include <fstream>
 #include <iostream>
 #include <string>
-#include <fstream>
 #include <vector>
-#include <algorithm>
 
 int main() {
 
+  std::ifstream fid;
+  fid.open("input.txt");
+  std::string line;
+  std::vector<double> elfscore;
+  double score = 0;
 
-    std::ifstream fid;
-    fid.open("C:\\Users\\N13617\\OneDrive - NGC\\Documents\\CPP\\aoc\\day1\\input.txt");
-    std::string line;
-    std::vector<double> elfscore;
-    double score = 0;
-
-    if (fid.is_open()) {
-        while (fid) {
-            std::getline (fid,line);
-            if (line.empty()) {
-                elfscore.push_back(score);
-                // std::cout << score << std::endl;
-                score = 0;
-            }
-            else {
-            score += std::stod(line);
-            }
-        }
+  if (fid.is_open()) {
+    while (fid) {
+      std::getline(fid, line);
+      if (line.empty()) {
+        elfscore.push_back(score);
+        // std::cout << score << std::endl;
+        score = 0;
+      } else {
+        score += std::stod(line);
+      }
     }
-    else
-    {
-        std::cout << "Couldn't open file\n";
+  } else {
+    std::cout << "Couldn't open file\n";
+  }
+  size_t len = elfscore.size();
+  std::sort(elfscore.begin(), elfscore.end());
+  std::cout << "Top 3 Elves: [";
+  for (int i = 0; i < 3; i++) {
+    if (i != 2) {
+      std::cout << elfscore[len - i - 1] << ", ";
+    } else {
+      std::cout << elfscore[len - i - 1] << "]";
     }
-    size_t len = elfscore.size(); 
-    std::sort(elfscore.begin(), elfscore.end());
-    std::cout << elfscore[len-1] << std::endl;
-    std::cout << elfscore[len-2] << std::endl;
-    std::cout << elfscore[len-3] << std::endl;
-    std::cout << elfscore[len-1] + elfscore[len-2] + elfscore[len-3] << std::endl;
-    return 0;
+  }
+  std::cout << std::endl;
+  std::cout << "Top 3 Combined: "
+            << elfscore[len - 1] + elfscore[len - 2] + elfscore[len - 3]
+            << std::endl;
+  return 0;
 }
